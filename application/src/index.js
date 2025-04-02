a78bff3da3f921b594512d15007061e17182a27a
Neeraj Ghatage <105358654+neerajghatage@users.noreply.github.com>
2025-04-02 10:48:38 +0530

    Update index.js

--- application/src/index.js
+++ application/src/index.js
@@ -33,7 +33,7 @@
     client.connect()
         .then(() => {
             db = client.db(process.env.MONGO_DB_NAME);
-            console.log("Connected to t MongoDB Atlas");
+            console.log("Connected to th MongoDB Atlas");
         })
         .catch(err => console.error("MongoDB connection error:", err));
 
